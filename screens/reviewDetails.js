d8322adcf86d9a5227c80cf9e9122706f66cb18d
Raywire <ryanwire@outlook.com>
2020-01-16 13:50:53 +0300

    Passing data between screens

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { globalStyles } from '../styles/global'
 
 export default function ReviewDetails({ navigation }) {
-  const pressHandler = () => {
-    navigation.goBack();
-  }
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails Screen</Text>
-      <Button title='back to homescreen' onPress={pressHandler} />
+      <Text>{ navigation.getParam('title') }</Text>
+      <Text>{ navigation.getParam('body') }</Text>
+      <Text>{ navigation.getParam('rating') }</Text>
     </View>
   );
 }
