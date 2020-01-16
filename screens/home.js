0f1c64bfa37a8ae567c7848fde758eaec43dbf76
Raywire <ryanwire@outlook.com>
2020-01-16 17:08:48 +0300

    Adds a modal

--- screens/home.js
+++ screens/home.js
@@ -1,16 +1,38 @@
 import React, { useState } from 'react';
-import { FlatList, View, TouchableOpacity, Text } from 'react-native';
+import { FlatList, View, TouchableOpacity, Text, Modal, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 import { globalStyles } from '../styles/global'
 import Card from '../shared/card';
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
     { title: 'Gotta Catch Them all (again)', rating: 4, body: 'lorem ipsum', key: '2' },
     { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
-  ])
+  ]);
+
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType='slide'>
+        <View style={styles.modalContent}>
+          <MaterialIcons 
+            name='close'
+            size={24} 
+            style={{...styles.modalToggle, ...styles.modalClose}} 
+            onPress={() => setModalOpen(false)} 
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons 
+        name='add' 
+        size={24} 
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)} 
+      />
+
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -24,3 +46,23 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  }
+});
